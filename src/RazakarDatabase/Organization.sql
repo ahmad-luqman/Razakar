f14a3e891f4e898e9a2af99fd008b735e8272f62
Aamir Butt <aamirbutt@gmail.com>
2018-12-08 14:33:13 +0500

    Added Database Projecte

---
+++ src/RazakarDatabase/Organization.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Organization]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(MAX) NULL, 
+    [RepName] VARCHAR(MAX) NULL, 
+    [Location] VARCHAR(MAX) NULL, 
+    [Email] VARCHAR(MAX) NULL, 
+    [PhoneNumber] VARCHAR(MAX) NULL, 
+    [Rating] DECIMAL(5, 2) NULL
+)
