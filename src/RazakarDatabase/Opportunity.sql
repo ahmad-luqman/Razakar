91c28ef717296a05ef5bed99db534e769ac66326
Aamir Butt <aamirbutt@gmail.com>
2018-12-08 16:36:14 +0500

    Added Opportunity table to db

---
+++ src/RazakarDatabase/Opportunity.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Opportunity]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [OrganizationName] VARCHAR(MAX) NULL, 
+    [Date] DATETIME NULL, 
+    [Time] DATETIME NULL, 
+    [Duration] DECIMAL(5, 2) NULL, 
+    [Details] VARCHAR(MAX) NULL
+)
